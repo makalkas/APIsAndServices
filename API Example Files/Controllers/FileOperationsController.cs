90c4c2aeaffcdf61e9955f809bbfc057d2dc56c0
MICHAEL KALKAS <71030672+makalkas@users.noreply.github.com>
2024-12-11 18:14:24 -0500

    Added Example Controllers

---
+++ API Example Files/Controllers/FileOperationsController.cs
@@ -0,0 +1,88 @@
+﻿using AmetekLabelAPI.Resources.Logging;
+using AmetekLabelAPI.Resources.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmetekLabelAPI.Controllers
+{
+    /// <summary>
+    /// Service endpoint class for file operations.
+    /// </summary>
+    //[Route("ametekapi/fileoperations/")]
+    [Authorize]
+    [ApiController]
+    public class FileOperationsController : Controller
+    {
+        #region Enums
+
+        #endregion Enums
+        #region Declarations
+        private readonly ILoggingBroker _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IFileService _fileService;
+        #endregion Declarations
+        #region Constructors
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="configuration"></param>
+        /// <param name="fileService"></param>
+        public FileOperationsController(ILoggingBroker logger, IConfiguration configuration, IFileService fileService)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _fileService = fileService;
+        }
+        #endregion Constructors
+        #region Properties
+
+        #endregion Properties
+        #region Public Endpoints
+        /// <summary>
+        /// Gets the names of all available label templates in directory
+        /// </summary>
+        /// <returns>List of template file names</returns>
+        [HttpGet("ametekapi/fileoperations/{filePath}", Name = "GetLableTemplateNames")]
+        public async ValueTask<List<string>> GetLableTemplateNames(string filePath)
+        {
+            //Todo: remove the need for the file path. this should be set up in the configuration.
+
+            return await _fileService.GetLabelTemplateFileNames(filePath);
+
+        }
+
+        /// <summary>
+        /// (Delete) This method deletes a file. 
+        /// </summary>
+        /// <param name="fullpath">Windows folder path to file to be deleted</param>
+        /// <param name="name">Exact name of file to be deleted.</param>
+        /// <returns></returns>
+        [HttpDelete("ametekapi/fileoperations/{fullpath}/{name}")]
+        public bool DeleteTemplate(string fullpath, string name)
+        {
+
+            return _fileService.DeleteTemplate(fullpath, name);
+        }
+
+
+        /// <summary>
+        /// This method renames a file or moves it to a new directory.
+        /// </summary>
+        /// <param name="CurrentFileName">The current file name including the full path.</param>
+        /// <param name="NewFileName">The new file name with the current full file path.</param>
+        /// <returns>Boolean value of true if operation was successful.</returns>
+        [HttpPost("ametekapi/fileoperations/{CurrentFileName}/{NewFileName}")]
+        public bool UpdateTemplateName(string CurrentFileName, string NewFileName)
+        {
+            return _fileService.UpdateTemplateName(CurrentFileName, NewFileName);
+
+        }
+        #endregion Public Endpoints
+        #region Private Methods
+
+        #endregion Private Methods
+
+
+    }
+}
---
+++ API Example Files/Controllers/FontsController.cs
@@ -0,0 +1,52 @@
+﻿using AmetekLabelAPI.Resources.Logging;
+using AmetekLabelAPI.Resources.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Versioning;
+
+namespace AmetekLabelAPI.Controllers
+{
+    /// <summary>
+    /// This controller is for handling server font information.
+    /// </summary>
+    [SupportedOSPlatform("windows")]
+    [Authorize]
+    [ApiController]
+    //[Route("ametekapi/[controller]")]
+    public class FontsController : Controller
+    {
+        #region Declarations
+        IFontService? _fontService = null;
+        private readonly ILoggingBroker _logger;
+        private readonly IConfiguration _configuration;
+
+        #endregion Declarations
+
+        #region Constructors
+        /// <summary>
+        /// Blank constructor for class object.
+        /// </summary>
+        /// <param name="logger">Application logger object</param>
+        /// <param name="configuration">Application configuration object</param>
+        /// <param name="fontService"></param>
+        public FontsController(ILoggingBroker logger, IConfiguration configuration, IFontService fontService)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _fontService = fontService;
+        }
+        #endregion Constructors
+
+        #region Public Endpoints
+        /// <summary>
+        /// Gets all fonts available on server.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/ametekapi/fonts", Name = "GetServerFonts")]
+        public List<string> GetServerFonts()
+        {
+            return _fontService!.GetFonts();
+        }
+        #endregion Public Endpoints
+    }
+}
